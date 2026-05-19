42304b09adfe55e34554647f62a9eb60bd6e6d8a
zedutra <zedutra@protonmail.com>
2026-05-18 22:18:51 -0300

    removendo projetos do repositorio

--- projetos/calculadora-sasi/script.js
+++
@@ -1,9 +0,0 @@
-function sasi() {
-    var n1 = window.document.querySelector('input#n1').value;
-    var n2 = window.document.querySelector('input#n2').value;
-    var n3 = window.document.querySelector('input#n3').value;
-    var res = window.document.querySelector('p#resultado');
-    var nota = (n1 * 0.35) + (n2 * 0.35) + (n3 * 0.3);
-
-    res.innerHTML = `<option>===== Você tirou: ${nota} pontos =====</option>`;
-}
